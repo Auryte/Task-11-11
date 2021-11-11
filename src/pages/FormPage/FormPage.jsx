e0c97fa2abf3b7d57957cd766f95ed1780cfc90a
Auryte <aureja@yahoo.com>
2021-11-11 11:20:28 +0200

    Initial: Academy task

--- src/pages/FormPage/FormPage.jsx
+++ src/pages/FormPage/FormPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import FormInput from '../../components/FormInput';
 import Main from '../../components/Main';
 import './formPage.css';
